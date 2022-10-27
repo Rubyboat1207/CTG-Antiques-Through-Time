089d0f31fd93d2b6eb53da1aec4cfd85eee1f471
Rubyboat <rudy@rudysoliz.com>
2022-10-27 06:42:34 -0500

    Lantern Test

--- Assets/HumanoidAnimator.cs
+++ Assets/HumanoidAnimator.cs
@@ -3,36 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class ReadOnlyAttribute : PropertyAttribute
-    {
-    
-    }
-    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-    public class ReadOnlyDrawer : PropertyDrawer
-    {
-        public override float GetPropertyHeight(SerializedProperty property,
-                                                GUIContent label)
-        {
-            return EditorGUI.GetPropertyHeight(property, label, true);
-        }
-    
-        public override void OnGUI(Rect position,
-                                    SerializedProperty property,
-                                    GUIContent label)
-        {
-            GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label, true);
-            GUI.enabled = true;
-        }
-    }
-
 [RequireComponent(typeof(Animator))]
 public class HumanoidAnimator : MonoBehaviour
 {
     [Header("Debug Info: ")]
-    [ReadOnly] [SerializeField] Vector3 lastPos;
-    [ReadOnly] [SerializeField] Vector3 curPos;
-    [ReadOnly] [SerializeField] Vector3 positionDelta;
+    [SerializeField] Vector3 lastPos;
+    [SerializeField] Vector3 curPos;
+    [SerializeField] Vector3 positionDelta;
     Animator animator;
 
     private void Start() {
